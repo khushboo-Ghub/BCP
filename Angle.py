243ca455eb7ba0cd3d9aadaf351b22683939e5a3
khushboo <khushbooagrawal748@gmail.com>
2025-08-19 11:59:41 +0530

    Add files via upload

---
+++ Angle.py
@@ -0,0 +1,13 @@
+# 7. Read three angles of triangles and determine their types(Right triangle, Obtuse triangle, Acute triangle).
+
+ang1 = int(input("enter angle1: "))
+ang2 = int(input("enter angle2: "))
+ang3 = int(input("enter angle3: "))
+
+if(ang1 > 90 or ang2 > 90 or ang3 > 90):
+    print("this triangle is obtuse triangle")
+elif(ang1 == 90 or ang2 == 90 or ang3 == 90):
+    print("triangle is obtuse triangle")
+else:
+    print("actute angle")
+
