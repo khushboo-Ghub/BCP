02c4d8c44cdc28231593e77625cb5bd9e7d59ae8
khushboo <khushbooagrawal748@gmail.com>
2025-08-19 12:06:11 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,14 @@
+# given 5 numbers A,B,C,D,E as input. print the avg of these 5 numbers
+
+a = int(input("enter number 1: "))
+b = int(input("number 2: "))
+c = int(input("number 3: "))
+d = int(input("number 4: "))
+e = int(input("number 5: "))
+
+sum = a+b+c+d+e
+print(sum)
+
+avg = sum/5
+
+print("avg of 5 numbers are: ", avg)
