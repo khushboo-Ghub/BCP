16bdd9b340bcc1b1e6c71374107b44390aa71d8b
khushboo <khushbooagrawal748@gmail.com>
2025-08-19 12:30:21 +0530

    eligibilityToVote.py

---
+++ eligibilityToVote.py
@@ -0,0 +1,9 @@
+# WAP to check whether a person is eligible to vote or not. 
+# Take input
+age = int(input("Enter age: "))
+
+# Check eligibility
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
