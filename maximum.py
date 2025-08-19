a61eeadd3a969ed4b2059cbb713185bec7dfd29a
khushboo <khushbooagrawal748@gmail.com>
2025-08-19 12:33:59 +0530

    maximum.py

---
+++ maximum.py
@@ -0,0 +1,9 @@
+# print the maximum
+#take input from user
+a = int(input("number 1: "))
+b = int(input("number 2: "))
+#check maximum between two numbers using if-else condition
+if(a>b):
+    print(f"{a} is the maximum number")
+else:
+print(f"{b} is the maximum number")
